06a6848cd51577810280c986b07a9f843438b30f
NeQIS <reconstructionbyneqis@gmail.com>
2025-07-17 13:19:33 +0530

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -5,7 +5,8 @@
  * Copyright 2010, John Resig
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
- *
+
+ 
  * Includes Sizzle.js
  * http://sizzlejs.com/
  * Copyright 2010, The Dojo Foundation
